4c09d5ebcc92be0124df5e088cbf8f4434dde6bb
Will Gertsch <willgertsch+github@gmail.com>
2021-07-02 10:45:16 -0700

    Don't seem to need to use a pseudo inverse in sensitivity function

--- src/ch_logistic.m
+++ src/ch_logistic.m
@@ -57,7 +57,7 @@ function y = ch_logistic(z, vars, beta, opt, p)
     % switch optimality
     if (opt == 'D')
         if lbeta == 3 && ~isempty(p)
-            y = g * [1 z^p1 z^p2] * pinv(M) * [1;z^p1;z^p2] - 3;
+            y = g * [1 z^p1 z^p2] * (M \ [1;z^p1;z^p2]) - 3;
         elseif lbeta == 3
             y = g * [1 z z^2] * (M \ [1;z;z^2]) - 3;
         else
